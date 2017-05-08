2716a3455ce096a2c25a1713c701193d6e33a6fe
mitchey <mitchey@localhost>
2017-05-09 01:21:07 +0900

    convert code for tmp.html into new style

--- WebContent/js/picture.js
+++ WebContent/js/picture.js
@@ -187,116 +187,28 @@ app.component("pictureList", {
 	controller: ["pageInfo", "showPicture", "$http", PictureListController]
 });
 
+class DownloadController {
+	
+	constructor(pageInfo, $http) {
+		this.pageInfo = pageInfo;
+		this.$http = $http;
+	} 
+	
+	download(form) {
+		const urls = this.urlLines.split(/\r\n|\r|\n/);
+		const ctrl = this;
+		this.$http.post("../resources/folder/download_tmp/" + this.pageInfo.currentFolder, urls).
+		then(function(response) {
+			ctrl.urlLines = "";
+			form.$setPristine();
+			form.$setUntouched();
+		}, function(response) {
+			$window.alert("failed image downloading");
+		});
+	}
+}
 
-
-//angular.module('pictureApp', [])
-//.controller('pictureController', ['$scope', '$http', '$window', function($scope, $http, $window) {
-//	
-//	$scope.init = function(infoListBase, picBase, folderListUrl) {
-//		$scope.infoListBase = infoListBase;
-//		$scope.picBase = picBase;
-//		$scope.curFolder = "";
-//		$scope.curPicture = 0;
-//		if ((folderListUrl != undefined) && (folderListUrl != null)) {
-//			getFolders(folderListUrl);
-//		}
-//		$scope.menuHeight = document.getElementById("menu-fix").clientHeight;
-//		$scope.contentsHeight = ($window.innerHeight - $scope.menuHeight) + "px";
-//		angular.element($window).on('load resize', function() {
-//			$scope.contentsHeight = ($window.innerHeight - $scope.menuHeight) + "px";
-//		});
-//	};
-//	
-//	$scope.showFolder = function(folder) {
-//		$http.get($scope.infoListBase + folder).
-//	    then(function(data, status, headers, config) {
-//	    	var infos = [];
-//	    	for (key in data) {
-//	    		var value = data[key];
-//	    		if (value.created) {
-//	    			value.created = new Date(value.created);
-//	    		}
-//	    		if (value.downloaded) {
-//	    			value.downloaded = new Date(value.downloaded);
-//	    		}
-//	    		value.id = key
-//	    		infos.push(value);
-//	    	}
-//			$scope.infoList = infos.sort(function(p1, p2) {
-//				return p2.downloaded - p1.downloaded; 
-//			});
-//    		$scope.curFolder = folder;
-//    		$scope.curPicture = 0;
-//	    });
-//	};
-//	
-//	$scope.showPicture = function(event, index) {
-//
-//		var picInfo = $scope.infoList[index];
-//		showPic(picInfo.id);
-//		
-//		$scope.curPicture = index;
-//		
-//		event.stopPropagation();
-//	};
-//	
-//	$scope.submitAfterReset = function(form) {
-//		var urls = $scope.urlLines.split(/\r\n|\r|\n/);
-//		$http.post("../resources/folder/download_tmp/" + $scope.curFolder, urls).
-//		then(function(data, status, config) {
-//			$scope.urlLines = "";
-//			form.$setPristine();
-//			form.$setUntouched();
-//		}, function(data, status, headers, config) {
-//			$window.alert("failed image downloading")
-//		});
-//	};
-//	
-//	$scope.nextPic = function() {
-//		changePic(1);
-//	};
-//	$scope.prevPic = function() {
-//		changePic(-1);
-//	};http://localhost/picture/html/folder.html
-//	$scope.startPic = function() {
-//		showPic($scope.infoList[0].id);
-//		$scope.curPicture = 0;
-//	};
-//	$scope.endPic = function() {
-//		var last = $scope.infoList.length - 1;
-//		showPic($scope.infoList[last].id);
-//		$scope.curPicture = last;
-//	};
-//	
-//	function getFolders(url) {
-//		$http.get(url).
-//	    then(function(data, status, headers, config) {
-//	    	$scope.folderList = data;
-//	    });
-//	};
-//	
-//	function changePic(r) {
-//		var dest = $scope.curPicture + r;
-//		if ((dest < 0) || (dest >= $scope.infoList.length)) {
-//			return;
-//		}
-//		
-//		var picInfo = $scope.infoList[dest];
-//		showPic(picInfo.id);
-//		$scope.curPicture = dest;
-//	};
-//	
-//	function showPic(id) {
-//		var child = $window.open("", "pic");
-//		var width = child.innerWidth;
-//		var height = child.innerHeight;
-//		child.onfocus = function(event) {
-//			child.blur();
-//			this.focus();
-//		}
-//		
-//		var url = $scope.picBase + id + (((width == 0) || (height == 0)) ? "" : ("?width=" + width + "&height=" + height));
-//		
-//		$window.open(url, "pic");
-//	}
-//}]);
+app.component("downloadForm", {
+	templateUrl: "../html/template/download_form.html",
+	controller: ["pageInfo", "$http", DownloadController]
+})
